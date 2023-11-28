a5c5da5fea31ca2dee112341a41a42ab12434c37
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-11-28 11:10:53 +0300

    TODO-5 ::COMPLETEDD: Override onCreatePreferences and add the preference xml file using addPreferencesFromResource

--- app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -1,11 +1,18 @@
 package com.example.android.sunshine;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
-    public void onCreatePreferences(Bundle bundle, String s) {
+    public void onCreatePreferences(Bundle bundle, String key) {
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
     }
 }
