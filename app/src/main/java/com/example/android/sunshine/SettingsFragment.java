ca78a2085ede917e398751473449560c97b7b089
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-11-28 23:08:43 +0300

    TODO-8,9,10,11,12 are done

--- app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -2,18 +2,76 @@ package com.example.android.sunshine;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+import java.util.List;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements
         SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle bundle, String key) {
         addPreferencesFromResource(R.xml.pref_general);
+
+        // COMPLETED (9) Set the preference summary on each preference that isn't a CheckBoxPreference
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            if (!(preference instanceof CheckBoxPreference)) {
+                setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+            }
+        }
+    }
 
+    // COMPLETED (13) Unregister SettingsFragment (this) as a SharedPreferenceChangedListener in onStop
+    @Override
+    public void onStop() {
+        super.onStop();
+        /* Unregister the preference change listener */
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    // COMPLETED (12) Register SettingsFragment (this) as a SharedPreferenceChangedListener in onStart
+    @Override
+    public void onStart() {
+        super.onStart();
+        /* Register the preference change listener */
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        // Checks if preference is instance of Preference,
+        if (preference instanceof ListPreference) {
+            // if it's cast it
+            ListPreference listPreference = (ListPreference) preference;
+            // now we need to find the  current index which selected
+            int prefIndex = listPreference.findIndexOfValue(value.toString());
+            if (prefIndex >= 0) {
+                // checks if the index is valid we will use set summary method
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            } else {
+                // for the other preference, set the summary to the value's simple string representation
+                preference.setSummary(value.toString());
+            }
+        }
     }
 
 }
